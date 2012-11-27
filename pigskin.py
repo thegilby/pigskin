f88e65cd040277ac33ad5d9e30da4e3c5c6e8e70
Andrew Chao <andrew.c.chao@ischool.berkeley.edu>
2012-11-27 02:05:16 -0800

    Pysolr
    I got pysolr working. I can query, add, remove indexes to solr.
    Still need to figure out to do more with it.

--- pigskin.py
+++ pigskin.py
@@ -3,6 +3,7 @@ import sqlite3
 from contextlib import closing
 from flask import Flask, request, session, redirect, url_for, \
      abort, render_template, flash
+from pysolr import Solr
 
 # configuration
 DEBUG = True
@@ -16,6 +17,28 @@ app.config.from_object(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/search/")
+def search():
+    # do stuff with solr here. 
+    print "im doing stuff"
+    conn = Solr('http://127.0.0.1:8983/solr/')
+    results = conn.search('tweet:"/@/"')
+    for result in results:
+        print result['name']
+
+    docs = [
+            {'id': 'testdoc.5', 'cat': ['poetry', 'science'], 'name': 'document 5', 'text': u''},
+            {'id': 'testdoc.6', 'cat': ['science-fiction',], 'name': 'document 6', 'text': u''},
+        ]
+
+    conn.add(docs)
+    results = conn.search('cat:"science"')
+    for result in results:
+        print result['name']
+
+    return render_template('search.html')
+
 
 @app.route("/about/")
 def about():
