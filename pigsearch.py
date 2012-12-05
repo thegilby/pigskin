5f6cbcc061927f40248430b08fa435d48a27b172
Andrew Chao <andrew.c.chao@ischool.berkeley.edu>
2012-12-04 23:27:19 -0800

    Tweets per second
    added variaitons of tweets per second
    
    getAllTweetsPerTeam() - provides a simple count per team
    getTweetsPerSecondPerTeam() - provides a dict of tweetCount per second for specified team
    getTweetsPerSecond() - a more versatile time specified request
    outputScript() - generates multiple json files for future use.
    
    also removed some unneeded code

--- pigsearch.py
+++ pigsearch.py
@@ -2,6 +2,9 @@
 # This is essential a wrapper for our specific data set
 
 from pysolr import Solr
+from datetime import datetime, timedelta
+
+import json
 
 # column_name data_type
 # date_time     date
@@ -11,6 +14,41 @@ from pysolr import Solr
 # hashtags      text
 # fb_weight     int
 # fb_assoc      text
+
+team_abbrev = [
+    'buf',
+    'mia',
+    'ne',
+    'nyj',
+    'bal',
+    'cin',
+    'cle',
+    'pit',
+    'hou',
+    'ind',
+    'jac',
+    'ten',
+    'den',
+    'kc',
+    'oak',
+    'sd',
+    'dal',
+    'nyg',
+    'phi',
+    'was',
+    'chi',
+    'det',
+    'gb',
+    'min',
+    'atl',
+    'car',
+    'no',
+    'tb',
+    'ari',
+    'stl',
+    'sf',
+    'sea'
+]
 
 class FootballIndex:
     
@@ -30,11 +68,93 @@ class FootballIndex:
         tweets['totalHits'] = results.hits
         return tweets
 
-    def getTweetsPerSecond(self, date, column='all'):
+
+
+    def getTweetsPerSecond(self, date='2012-10-21T10:00:00', column='football'):
         result = {}
-        # if column == 'all':
-        return result   
-
-
+        startdate = datetime.strptime(date,'%Y-%m-%dT%H:%M:%S')
+        enddate = startdate + timedelta(seconds=60*60*13) # 13 hrs later
         
+        params = {  'facet.field' : ['fb_assoc'],
+                    'facet.range' : ['date_time'],
+                    'facet.range.start' : date + 'Z',
+                    'facet.range.end' : enddate.isoformat() + 'Z',
+                    'facet.range.gap':'+1000MILLISECOND',
+                    'facet.mincount' : '1',
+                    'facet.sort' : 'index'
+                    }
+        
+        counts = {}
+        if column == 'all':
+            dataset = self.conn.search("*:*",facet='on',
+                ** params)
+            print dataset.hits
+            counts = dataset.facets['facet_ranges']['date_time']['counts']
+            result = convertToDict(counts)
+        elif column == 'football':
 
+            dataset = self.conn.search("fb_weight:[2 TO *]",facet='on',
+                ** params)
+            for data in dataset:
+                print data
+            print dataset.hits
+            print dataset.facets['facet_fields']
+            counts = dataset.facets['facet_ranges']['date_time']['counts']
+            result = convertToDict(counts)
+
+        return result
+
+    # returns a dictionary of teamname:count
+    def getAllTweetsPerTeam(self):
+        params = {  'facet.field' : ['fb_assoc'],
+                    'facet.sort' : 'index'
+                    }
+        dataset = self.conn.search("fb_weight:[2 TO *]",facet='on',
+                ** params)
+        
+        return convertToDict(dataset.facets['facet_fields']['fb_assoc'])
+
+    # Get tweets per second for a given team on a given day.
+    # Input:
+    #   team - official abbreviated teamname
+    #   date - %Y-%m-%dT%H:%M:%S format 2012-10-21T10:00:00
+    # returns a dictionary of teamname:count
+    def getTweetsPerSecondPerTeam(self, team, date):
+        result = {}
+        startdate = datetime.strptime(date,'%Y-%m-%dT%H:%M:%S')
+        enddate = startdate + timedelta(seconds=60*60*13) # 13 hrs later
+        
+        params = {'facet.range' : ['date_time'],
+                    'facet.range.start' : date + 'Z',
+                    'facet.range.end' : enddate.isoformat() + 'Z',
+                    'facet.range.gap':'+1000MILLISECOND',
+                    'facet.mincount' : '1',
+                    'facet.sort' : 'index'
+                    }
+
+        dataset = self.conn.search("fb_weight:[2 TO *]",fq='fb_assoc:'+team,
+            facet='on', ** params)
+
+        print dataset.hits
+        counts = dataset.facets['facet_ranges']['date_time']['counts']
+        result = convertToDict(counts)
+
+        return result
+
+
+def outputScript():
+    index = FootballIndex()# write out multiple json files
+
+    # football
+    for team in team_abbrev:
+        tweets = index.getTweetsPerSecondPerTeam(team,'2012-10-21T10:00:00')
+        f = open("tweetsPerSecond_" + team + ".json","w")
+        f.write(json.dumps(tweets,indent=4))
+    return
+
+# Helper
+def convertToDict(countList):
+    result = {}
+    for key, value in zip(countList[::2],countList[1::2]):
+        result[key] = value
+    return result
--- pigskin.py
+++ pigskin.py
@@ -40,30 +40,21 @@ def team(team=None):
 def search():
     # do stuff with solr here.
     print "im doing stuff"
-    conn = Solr('http://127.0.0.1:8983/solr/')
-    results = conn.search('cat:"/@/"')
-    for result in results:
-        print result['name']
-
-    docs = [
-            {'id': 'testdoc.5', 'cat': ['poetry', 'science'], 'name': 'document 5', 'text': u''},
-            {'id': 'testdoc.6', 'cat': ['science-fiction',], 'name': 'document 6', 'text': u''},
-        ]
-
-    conn.add(docs)
-    results = conn.search('science')
-    for result in results:
-        print result
-
-    return render_template('search.html', results=docs)
+    
+    football = FootballIndex()
+    # print football.getAllTweetsPerTeam()
+    # print football.getTweetsPerSecond()
+    print football.getTweetsPerSecondPerTeam('sf','2012-10-21T10:00:00')
+    return render_template('search.html')
 
 @app.route("/searchsolr")
 def searchsolr():
     query = request.args.get('query','')
     print query
-    football = FootBallIndex()
+    football = FootballIndex()
     # rows='10' on default
     results = football.search(query)
+
     # print data    # data = jsonify({'bar': ('baz', query, 1.0, 2)})
     return jsonify(results)
 
